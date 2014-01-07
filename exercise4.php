60f428337935cb67fe53188ac6809cb7729e86d3
Filipe Mendonça <filipesm_7@hotmail.com>
2014-01-07 19:37:46 +0000

    stuff

--- exercise4.php
+++ exercise4.php
@@ -1,15 +1,35 @@
 <?php
 
-$data = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Ponta%20Delgada,Portugal'), TRUE);
+$data = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Ponta%20Delgada,Portugal&units=metric'), TRUE);
 
-//var_dump($data);
+echo sprintf("The temperature for today is %d, and it is %s.", get_key_value($data, 'temp'), get_key_value($data, 'description'));
 
-echo array_to_str($data);
-
-echo '<br />';
-echo 'Azores: ' . format_date(time(), 'Atlantic/Azores') . ' | Toronto: ' . format_date(time(), 'America/Toronto');
+echo '<br/>Azores: ' . format_date(time(), 'Atlantic/Azores') . ' | Toronto: ' . format_date(time(), 'America/Toronto');
 
 /* FUNCTIONS */
+
+function get_key_value($arr, $nkey){
+	$val="";	
+	foreach($arr as $key => $value){
+		if($key == $nkey && !is_array($value)){
+			return $value;
+		}
+		elseif(is_array($value)){
+			$val = get_key_value($value, $nkey);
+			if(!empty($val))
+				break;
+		}
+	}
+	return $val;
+}
+
+function format_date($date_time, $timezone, $date_format='Y-m-d H:i:s'){
+	$date = new DateTime();
+	$date->setTimestamp($date_time);
+
+	$date->setTimezone(new DateTimeZone($timezone));
+	return $date->format($date_format);		
+}
 
 function array_to_str($arr){
 	$str = "";
@@ -21,13 +41,5 @@ function array_to_str($arr){
 			$str .=  '<li>' . $key . ': ' . $value . '</li>';; 
 	}
 	return $str;
-}
-
-function format_date($date_time, $timezone, $date_format='Y-m-d H:i:s'){
-	$date = new DateTime();
-	$date->setTimestamp($date_time);
-
-	$date->setTimezone(new DateTimeZone($timezone));
-	return $date->format($date_format);		
 }
 ?>
